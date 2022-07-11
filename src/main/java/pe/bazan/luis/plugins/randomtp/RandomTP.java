8d0ee4c399eef3120ca9eccb73ac3a99cb8e8c5b
Luis Bazán <luis@bazan.pe>
2022-07-11 11:42:43 -0500

    first comit

---
+++ src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
@@ -0,0 +1,17 @@
+package pe.bazan.luis.plugins.randomtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomTP extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}

e43565baa610efa16f9f4236d27528231e5047c3
Luis Bazán <luis@bazan.pe>
2022-07-11 12:52:16 -0500

    Economy setup

--- src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
+++ src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
@@ -1,17 +1,43 @@
 package pe.bazan.luis.plugins.randomtp;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class RandomTP extends JavaPlugin {
+  private static Economy econ = null;
 
   @Override
   public void onEnable() {
     // Plugin startup logic
-
+    if (!setupEconomy() ) {
+      getLogger().info("Disabled due to no Vault dependency found!");
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+    new RTPCommand(this);
   }
 
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+  }
+
+  private boolean setupEconomy() {
+    if (getServer().getPluginManager().getPlugin("Vault") == null) {
+      return false;
+    }
+    RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+    if (rsp == null) {
+      return false;
+    }
+    econ = rsp.getProvider();
+    return econ != null;
+  }
+
+  public static Economy getEcon() {
+    return econ;
   }
 }

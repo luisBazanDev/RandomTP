95c51363cb85f1d97dfb57828fbefa7b30e50b95
Luis Bazán <luis@bazan.pe>
2022-07-11 13:08:49 -0500

    Add custom yml system.

---
+++ src/main/java/pe/bazan/luis/plugins/randomtp/CustomYML.java
@@ -0,0 +1,102 @@
+package pe.bazan.luis.plugins.randomtp;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+
+public class CustomYML {
+  private String path = "plugins"+File.separatorChar;
+  private File ymlfile;
+  private FileConfiguration fc;
+  /**
+   *
+   * @param name of the file. can include folder paths (for instance
+   *             kits/users/Steve). Then the file Steve.yml would be in the folder
+   *             plugins/'plugin_name'/kits/users/Steve.yml
+   * @param jp   -> Your plugin main instance
+   */
+  public CustomYML(String name, JavaPlugin jp) {
+    this.path = "plugins/" + jp.getName();
+    this.ymlfile = new File(String.valueOf(path) + File.separatorChar + name + ".yml");
+    this.fc = (FileConfiguration) YamlConfiguration.loadConfiguration(this.ymlfile);
+  }
+  /**
+   * @throws ClassCastException
+   * @param <T>
+   * @param path -> path in this config
+   * @return Object if found, nullable
+   */
+  @SuppressWarnings("unchecked")
+  @Nullable
+  public <T> T getConfigField(String path) {
+    FileConfiguration cfg = this.getConfig();
+    Object o = cfg.get(path);
+    if (o != null) {
+      return (T) o;
+    } else {
+      cfg.set(path, "undefined");
+      save();
+    }
+    return null;
+  }
+  /**
+   *
+   * @param path of field
+   * @param o value as Object
+   * @return whether the save was successful
+   */
+  public boolean setField(String path, Object o) {
+    getConfig().set(path, o);
+    getConfig().contains("");
+    return save();
+  }
+  public boolean contains(String path) {
+    return getConfig().contains(path);
+  }
+  /**
+   * @param <T> -> Tries to cast the field value to the Class T
+   * @param whenNotFound -> What should get created in the config if field not exist
+   * @param path -> path in this config
+   * @return Object T if found, null if cast failed
+   */
+  @SuppressWarnings("unchecked")
+  @Nullable
+  public <T> T getConfigField(String path, T whenNotFound) {
+    FileConfiguration cfg = this.getConfig();
+    Object o = cfg.get(path);
+    if (o != null) {
+      try {
+        return (T) o;
+      } catch (Exception e) {
+        return null;
+      }
+    } else {
+      cfg.set(path, whenNotFound);
+      save();
+    }
+    return whenNotFound;
+  }
+  private FileConfiguration getConfig() {
+    return this.fc;
+  }
+  /**
+   *
+   * @return true if the save was successful, otherwise false
+   */
+  private boolean save() {
+    File folder = new File(path);
+    if (!folder.exists())
+      folder.mkdirs();
+    try {
+      this.fc.save(this.ymlfile);
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+}
--- src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
+++ src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
@@ -4,13 +4,15 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public final class RandomTP extends JavaPlugin {
   private static Economy econ = null;
+  private CustomYML messages = null;
+  private CustomYML config = null;
+  private CustomYML db = null;
 
   @Override
   public void onEnable() {
+    reloadConfigs();
     // Plugin startup logic
     if (!setupEconomy() ) {
       getLogger().info("Disabled due to no Vault dependency found!");
@@ -23,6 +25,12 @@ public final class RandomTP extends JavaPlugin {
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+  }
+
+  public void reloadConfigs() {
+    messages = new CustomYML("messages", this);
+    config = new CustomYML("config", this);
+    db = new CustomYML("db", this);
   }
 
   private boolean setupEconomy() {
@@ -39,5 +47,17 @@ public final class RandomTP extends JavaPlugin {
 
   public static Economy getEcon() {
     return econ;
+  }
+
+  public CustomYML getCustomConfig() {
+    return config;
+  }
+
+  public CustomYML getMessages() {
+    return messages;
+  }
+
+  public CustomYML getDb() {
+    return db;
   }
 }

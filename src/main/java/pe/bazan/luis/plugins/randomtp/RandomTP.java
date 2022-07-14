e008708261dbb6bb4aa4b6aa5c197945d5b96381
Luis Bazán <luis@bazan.pe>
2022-07-14 09:29:53 -0500

    Add method withdrawAmount

--- src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
+++ src/main/java/pe/bazan/luis/plugins/randomtp/RandomTP.java
@@ -1,9 +1,14 @@
 package pe.bazan.luis.plugins.randomtp;
 
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import pe.bazan.luis.plugins.randomtp.commands.RTP;
+
+import java.sql.Array;
 
 public final class RandomTP extends JavaPlugin {
   private static Economy econ = null;
@@ -45,6 +50,17 @@ public final class RandomTP extends JavaPlugin {
     }
     econ = rsp.getProvider();
     return econ != null;
+  }
+
+  public boolean withdrawAmount(CommandSender player, double amount) {
+    double balance = econ.getBalance((OfflinePlayer) player);
+    if(balance < amount) {
+      MessagesFormat.sendSenderWithPrefix(player, messages.getConfigField("money.error"), new String[]{String.valueOf(amount)});
+      return false;
+    }
+    econ.withdrawPlayer((OfflinePlayer) player, amount);
+    MessagesFormat.sendSenderWithPrefix(player, messages.getConfigField("money.success"));
+    return true;
   }
 
   public static Economy getEcon() {
